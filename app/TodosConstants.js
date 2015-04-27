c9bb07a6672c0c4a641fdceb9025a957e35cc0ce
phoeon <598731376@qq.com>
2015-04-28 01:22:11 +0800

    flux react todos

--- app/TodosConstants.js
+++ app/TodosConstants.js
@@ -2,5 +2,7 @@ module.exports = {
 	LOAD_All : "loadAll",
 	CREATE_TODO : "create",
 	UPDATE_TODO	: "update",
+	REMOVE_TODO : "remove",
+	UPDATE_ALL_TODO : "updateAll",
 	EVENT_NAME : "todo_change"
 };
